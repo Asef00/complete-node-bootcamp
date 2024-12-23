dd3160f7eefd99d6ba2fe6eec0c4939f0d35be56
Asef00 <asefhosseini00@gmail.com>
2024-12-23 13:34:30 +0330

    read file name form another file, concat texts from two files and write to another

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,20 +1,28 @@
 const fs = require("fs");
 
 //blocking code
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
 
 //non-blocking code
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  console.log(data);
+fs.readFile("./txt/start.txt", "utf-8", (err, fileName) => {
+  console.log("File name:", fileName);
 
-  const textOut = `This is what we know about the avocado: ${data}.\nCreated on ${Date.now()}`;
-
-  fs.writeFile("./txt/end.txt", textOut, (err) => {
-    console.log("File written!");
+  fs.readFile(`./txt/${fileName}.txt`, "utf-8", (err, mainContext) => {
+    console.log("Main context:", mainContext);
+    fs.readFile("./txt/append.txt", "utf-8", (err, appendData) => {
+      console.log("Append data:", appendData);
+      fs.writeFile(
+        `./generated/${Date.now()}.txt`,
+        `${mainContext}\n${appendData}`,
+        (err) => {
+          console.log("File written!");
+        }
+      );
+    });
   });
 });
