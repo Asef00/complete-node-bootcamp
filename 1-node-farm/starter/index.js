c3586e1b525ccef19247d908b155b1038d860696
Asef00 <asefhosseini00@gmail.com>
2024-12-23 18:06:57 +0330

    feat: template builders, utils and endpoints

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -36,19 +36,55 @@ const readFile = () => fs.readFile("./txt/start.txt", "utf-8", (err, fileName) =
 // readFile();
 
 const productData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const productTemplate = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const cardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+
+const dataObj = JSON.parse(productData);
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{{product.name}}/g, product.name);
+  output = output.replace(/{{product.image}}/g, product.image);
+  output = output.replace(/{{product.price}}/g, product.price);
+  output = output.replace(/{{product.from}}/g, product.from);
+  output = output.replace(/{{product.nutrients}}/g, product.nutrients);
+  output = output.replace(/{{product.quantity}}/g, product.quantity);
+  output = output.replace(/{{product.description}}/g, product.description);
+  output = output.replace(/{{product.unit}}/g, product.unit);
+  output = output.replace(/{{product.organic}}/g, !product.organic ? 'not-organic' : '');
+  output = output.replace(/{{product.id}}/g, product.id);
+  return output;
+};
 
 const createServer = () => {
   http.createServer((req, res) => {
     // res.end("Hello from the server!");
     const pathName = req.url;
+
+    //overview page
     if (pathName === "/overview" || pathName === "/") {
-      res.end("This is the overview");
-    } else if (pathName === "/product") {
-      res.end("This is the product");
-    } else if (pathName === "/api") {
+      res.writeHead(200, { "Content-type": "text/html" });
+      const cardsHtml = dataObj.map(el => replaceTemplate(cardTemplate, el)).join('');
+      const output = overviewTemplate.replace('{{products}}', cardsHtml);
+      res.end(output);
+    }
+    
+    //product page
+    else if (pathName.match(/^\/product/)) {
+      res.writeHead(200, { "Content-type": "text/html" });
+      const product = dataObj[req.query.id];
+      const output = replaceTemplate(productTemplate, product);
+      res.end(output);
+    }
+
+    //API
+    else if (pathName === "/api") {
       res.writeHead(200, { "Content-type": "application/json" });
       res.end(productData);
-    } else {
+    }
+
+    //not found
+    else {
       res.writeHead(404, { "Content-type": "text/html" });
       res.end("<h1>Page not found</h1>");
     }
