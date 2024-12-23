7207322fa3d7d84f8f31db2cdb9646ac08b8bb18
Asef00 <asefhosseini00@gmail.com>
2024-12-23 13:42:02 +0330

    make code modular

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
+const http = require("http");
 
 //blocking code
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
+const readFileSync = () => {
+  const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+  console.log(textIn);
 
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
+  const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+  fs.writeFileSync("./txt/output.txt", textOut);
+  console.log("File written!");
+};
 
 //non-blocking code
-fs.readFile("./txt/start.txt", "utf-8", (err, fileName) => {
+const readFile = () => fs.readFile("./txt/start.txt", "utf-8", (err, fileName) => {
   if (err) return console.log("Error reading file");
   console.log("File name:", fileName);
 
@@ -27,3 +30,6 @@ fs.readFile("./txt/start.txt", "utf-8", (err, fileName) => {
     });
   });
 });
+
+// readFileSync();
+// readFile();
